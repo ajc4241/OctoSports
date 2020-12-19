9c59146d7858624d82e4bb8b72996bc08b01f791
Alexis Chiang <ajc4241@g.rit.edu>
2020-12-19 15:30:19 -0500

    Made a bunch of HTML's for all the different octocats

--- app.py
+++ app.py
@@ -9,5 +9,32 @@ def home():
 @app.route("/cyclist")
 def cyclist():
     return render_template("cyclist.html")
+@app.route("/football")
+def football():
+    return render_template("football.html")
+@app.route("/hockey")
+def hockey():
+    return render_template("hockey.html")
+@app.route("/runner")
+def runner():
+    return render_template("runner.html")
+@app.route("/bunny")
+def bunny():
+    return render_template("bunny.html")
+@app.route("/scuba")
+def scuba():
+    return render_template("scuba.html")
+@app.route("/snowboard")
+def snowboard():
+    return render_template("snowboard.html")
+@app.route("/soccer")
+def soccer():
+    return render_template("soccer.html")
+@app.route("/swim")
+def swim():
+    return render_template("swim.html")
+@app.route("/panda")
+def panda():
+    return render_template("panda.html")
 if __name__ == "__main__":
     app.run()
